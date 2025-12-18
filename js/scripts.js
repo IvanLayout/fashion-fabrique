ccfea4690fed798d24294a65f05fa566a15513a0
VlaDSerga <65920800+VlaDSerga@users.noreply.github.com>
2025-12-18 11:03:24 +0300

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -350,10 +350,6 @@ $(() => {
 });
 
 $(window).on('load', () => {
-	if ($('.application__slider').length){
-		applicationSlider()
-	}
-
 	if ( $('.products__grid').length ) {
 		$('.products__grid').each(function() {
 			productsHeight($(this), parseInt($(this).css('--product-count')))
@@ -396,10 +392,6 @@ $(window).on('resize', () => {
 		}
 	}
 
-	if ($('.application__slider').length){
-		applicationSlider()
-	}
-
 	if ($('.product-images__slider').length){
 		productImagesSlider()
 	}
@@ -422,46 +414,6 @@ function productsHeight(context, step) {
 	}
 }
 
-function applicationSlider(){
-	if ( $(window).width() < 768 && !$('.application__slider').hasClass('swiper-initialized') ) {
-		$('.application__slider').addClass('swiper')
-		$('.application__items').addClass('swiper-wrapper').removeClass('_flex')
-		$('.application__item').addClass('swiper-slide')
-
-		approachSwiper = new Swiper('.application__slider', {
-			loop: false,
-			watchSlidesProgress: true,
-			watchOverflow: true,
-			spaceBetween: 10,
-			slidesPerView: 'auto',
-			preloadImages: false,
-			lazy: {
-				loadPrevNext: true,
-				elementClass: 'lazyload',
-				enabled: true,
-				loadedClass: 'loaded',
-				checkInView: true,
-				loadOnTransitionStart: true
-			},
-			navigation: {
-				nextEl: '.slider-button-next',
-				prevEl: '.slider-button-prev'
-			}
-		})
-	} else if ($(window).width() > 767 && $('.application__slider').hasClass('swiper-initialized')) {
-		if ($('.application__slider').length === 1 && $('.application__slider').hasClass('swiper-initialized')) {
-			approachSwiper.destroy(true, true)
-		} else if ($('.application__slider').length >= 2 && $('.application__slider').hasClass('swiper-initialized')) {
-			approachSwiper.forEach(function (element) {
-				element.destroy(true, true)
-			})
-		}
-
-		$('.application__slider').removeClass('swiper')
-		$('.application__items').removeClass('swiper-wrapper').addClass('_flex')
-		$('.application__item').removeClass('swiper-slide')
-	}
-}
 
 function productImagesSlider(){
 	if ( $(window).width() < 1024 && !$('.product-images__slider').hasClass('swiper-initialized') ) {
